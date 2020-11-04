81981f4f480873e938ce9e1b1cd495a37c51e17b
MotebangM <motebang.mokwatsi@palota.co.za>
2020-11-04 12:18:36 +0200

    task: home screen page NBcouldnt complete

---
+++ public/wookie/lib/views/home/pages/home_screen.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:wookie/utils/loading.dart';
+import 'package:wookie/view_models/movie_view_model.dart';
+
+class HomeScreen extends StatefulWidget {
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  void initState() {
+    Provider.of<MovieViewModel>(context, listen: false).getMovie();
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Text("Wookie Movies"),
+        actions: [],
+      ),
+      body: Consumer<MovieViewModel>(builder:
+          (BuildContext ctx, MovieViewModel movieViewModel, Widget myWidget) {
+        return SafeArea(child: checkStatus(movieViewModel));
+      }),
+    );
+  }
+
+  checkStatus(MovieViewModel movieViewModel) {
+    switch (movieViewModel.fetchState) {
+      case LoadingStatus.done:
+        return createList(movieViewModel);
+      case LoadingStatus.error:
+        return Container(child: Text("Error"));
+      case LoadingStatus.fetching:
+        return Center(child: CircularProgressIndicator());
+      case LoadingStatus.not_fetched:
+        return Container(child: Text("Error"));
+    }
+  }
+
+  createList(MovieViewModel movieViewModel) {
+    return Container(
+      margin: EdgeInsets.symmetric(vertical: 20),
+      height: 300,
+      child: ListView.builder(
+          itemCount: movieViewModel?.movieList?.length ?? 0,
+          scrollDirection: Axis.horizontal,
+          itemBuilder: (BuildContext context, int index) {
+            var item = movieViewModel?.movieList[index];
+            return Container(
+              width: 160,
+              child: Card(
+                  child: Wrap(
+                children: [Image.network(item.backdrop)],
+              )),
+            );
+          }),
+    );
+  }
+}
