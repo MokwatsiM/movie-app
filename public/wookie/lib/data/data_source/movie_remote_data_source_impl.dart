af8991564afc2c3b81a335860a788d8eb39c1415
MotebangM <motebang.mokwatsi@palota.co.za>
2020-11-04 10:30:19 +0200

    task: implemented remote data source impl to get the response and map to movie model

---
+++ public/wookie/lib/data/data_source/movie_remote_data_source_impl.dart
@@ -0,0 +1,16 @@
+import 'package:wookie/data/core/api_client.dart';
+import 'package:wookie/data/data_source/movie_remote_data_source.dart';
+import 'package:wookie/data/models/movie_model.dart';
+
+class MovieRemoteDataSourceImpl extends MovieRemoteDataSource {
+  final ApiClient _client;
+
+  MovieRemoteDataSourceImpl(this._client);
+
+  @override
+  Future<List<Movies>> getMovie() async {
+    final response = await _client.get();
+    List<Movies> movies = MovieModel.fromJson(response).movies;
+    return movies;
+  }
+}
